fadf3d9d053582e517b236fb69c9b9966e0e78f1
Tiago Lustosa Santos <tiagolustosasantos@hotmail.com>
2022-01-15 17:50:50 -0300

    Seguindo a documentaçao

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,41 @@ void main() {
   runApp(const MyApp());
 }
 
+Widget titleSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Row(
+    children: [
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: const Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Text(
+              'Kandersteg, Switzerland',
+              style: TextStyle(
+                color: Colors.grey[500],
+              ),
+            ),
+          ],
+        ),
+      ),
+      Icon(
+        Icons.star,
+        color: Colors.red[500],
+      ),
+      const Text('41'),
+    ],
+  ),
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -19,6 +54,27 @@ class MyApp extends StatelessWidget {
   }
 }
 
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(icon, color: color),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            color: color,
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -30,123 +86,45 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
-          body: Column(
-        children: [
-          Row(
-            children: [
-              Image.asset(
-                'assets/images/lake.jpg',
-                width: MediaQuery.of(context).size.width,
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              children: [
-                Row(
-                  children: const [
-                    Text(
-                      'Oeschinen Lake Campground',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [
-                    Icon(
-                      Icons.star,
-                      color: Colors.deepOrange,
-                    ),
-                    Text('41'),
-                  ],
-                ),
-                Row(
-                  children: const [
-                    Text(
-                      'Kandersteg, Switzerland',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+        body: ListView(
+          children: [
+            Image.asset(
+              'assets/images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                children: const [
-                  Icon(
-                    Icons.call,
-                    color: Colors.blue,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 8.0),
-                    child: Text(
-                      'CALL',
-                      style: TextStyle(color: Colors.blue, fontSize: 12),
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: const [
-                  Icon(
-                    Icons.router,
-                    color: Colors.blue,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 8.0),
-                    child: Text(
-                      'ROUTE',
-                      style: TextStyle(color: Colors.blue, fontSize: 12),
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: const [
-                  Icon(
-                    Icons.share,
-                    color: Colors.blue,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 8.0),
-                    child: Text(
-                      'SHARE',
-                      style: TextStyle(color: Colors.blue, fontSize: 12),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Column(
-            children: const [
-              Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: Text(
-                  'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-                  'Alps. Situated 1,578 meters above sea level, it is one of the '
-                  'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-                  'half-hour walk through pastures and pine forest, leads you to the '
-                  'lake, which warms to 20 degrees Celsius in the summer. Activities '
-                  'enjoyed here include rowing, and riding the summer toboggan run.',
-                  textAlign: TextAlign.justify,
-                ),
-              )
-            ],
-          )
-        ],
-      )),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
     );
   }
 }
